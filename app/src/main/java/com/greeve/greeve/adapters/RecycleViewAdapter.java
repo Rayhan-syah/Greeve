5c2db505a599d85f8e29da517bdda3adb84304c7
Rayhan <asrehan64@gmail.com>
2020-02-20 21:55:15 +0700

    detail

--- app/src/main/java/com/greeve/greeve/adapters/RecycleViewAdapter.java
+++ app/src/main/java/com/greeve/greeve/adapters/RecycleViewAdapter.java
@@ -1,15 +1,19 @@
 package com.greeve.greeve.adapters;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
 import com.greeve.greeve.R;
 import com.greeve.greeve.models.Article;
 
@@ -29,8 +33,15 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View v;
         v = LayoutInflater.from(mContext).inflate(R.layout.item_card_view_article,parent,false);
+        final MyViewHolder vHolder = new MyViewHolder(v);
+        vHolder.item_card_view_article.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(mContext,"coba"+String.valueOf(vHolder.getAdapterPosition()),Toast.LENGTH_LONG).show();
+            }
+        });
 
-        return new MyViewHolder(v);
+        return vHolder;
     }
 
     @Override
@@ -47,10 +58,12 @@ public class RecycleViewAdapter extends RecyclerView.Adapter<RecycleViewAdapter.
     public static class MyViewHolder extends RecyclerView.ViewHolder{
         TextView tv_title;
         ImageView img_article;
+        LinearLayout item_card_view_article;
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
             tv_title = (TextView)itemView.findViewById(R.id.title_article);
             img_article = (ImageView)itemView.findViewById(R.id.image_article);
+            item_card_view_article = (LinearLayout)itemView.findViewById(R.id.item_article_id);
         }
     }
 }
