46fd176d1d02da8e91f041f56c042565ec6c0d2f
Rayhan <asrehan64@gmail.com>
2020-02-21 23:31:00 +0700

    Hore

--- app/src/main/java/com/greeve/greeve/ui/greeve/article/ArticleFragment.java
+++ app/src/main/java/com/greeve/greeve/ui/greeve/article/ArticleFragment.java
@@ -47,7 +47,36 @@ public class ArticleFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         lstArticles = new ArrayList<>();
-        lstArticles.add(new Article("Hello","https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-mengurangi-sampah-popok-mengolahnya-menjadi-pupuk-e28094-zerowaste-id.png","https://greeve.law.blog/2020/02/21/mengurangi-sampah-popok-mengolahnya-menjadi-pupuk"));
+        lstArticles.add(new Article("Mengurangi Sampah Popok & Mengolahnya Menjadi Pupuk",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-mengurangi-sampah-popok-mengolahnya-menjadi-pupuk-e28094-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/mengurangi-sampah-popok-mengolahnya-menjadi-pupuk"));
+        lstArticles.add(new Article("Lerak sebagai alternatif deterjen dan sabun",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-lerak-sebagai-alternatif-deterjen-dan-sabun-e28094-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/lerak-sebagai-alternatif-deterjen-dan-sabun"));
+        lstArticles.add(new Article("DIY Deterjen Sehat",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-diy-deterjen-sehat-e28094-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/diy-deterjen-sehat"));
+        lstArticles.add(new Article("Jejak Karbon Apa sih ?",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-apa-itu-jejak-karbon-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/jejak-karbon-apa-sih"));
+        lstArticles.add(new Article("7 Simbol dan Jenis Plastik yang Perlu Kamu Ketahui",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-7-simbol-dan-jenis-plastik-yang-perlu-kamu-ketahui-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/7-simbol-dan-jenis-plastik-yang-perlu-kamu-ketahui"));
+        lstArticles.add(new Article("7 Simbol dan Jenis Plastik yang Perlu Kamu Ketahui",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-tentang-paper-cups-e28094-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/tentang-paper-cups/"));
+        lstArticles.add(new Article("Tentang Paper Cups",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-tentang-paper-cups-e28094-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/tentang-paper-cups/"));
+        lstArticles.add(new Article("Jawaban dari masalah sampah di Indonesia",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-jawaban-dari-masalah-sampah-di-indonesia-e28094-zerowaste-id.png",
+                "https://greeve.law.blog/2020/02/21/jawaban-dari-masalah-sampah-di-indonesia"));
+        lstArticles.add(new Article("Alternatif pengganti tisu (Sapu Tangan)",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-alternatif-pengganti-tisu-sapu-tangan-e28094-zerowaste-id.png",
+                "https://greevelaw.wordpress.com/2020/02/21/alternatif-pengganti-tisu-sapu-tangan/"));
+        lstArticles.add(new Article("Ecobricks",
+                "https://greevelaw.files.wordpress.com/2020/02/screenshot_2020-02-21-ecobricks-e28094-zerowaste-id4.png",
+                "https://greeve.law.blog/2020/02/21/ecobricks/"));
     }
 
 }
